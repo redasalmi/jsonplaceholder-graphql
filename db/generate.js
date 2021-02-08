d3417ceeb487a6522a179149752e752632c95d61
redasalmi <reda.salmi.elt@gmail.com>
2021-02-08 22:59:20 +0100

    added local database and script to generate it

---
+++ db/generate.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const fakeData = require('./fakeData');
+const adapter = new FileSync(path.join(__dirname, 'db.json'));
+const db = low(adapter);
+
+db.defaults(fakeData).write();
